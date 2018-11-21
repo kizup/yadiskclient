e1d72de3cd535fb3d10787eed8a5dabb788568bb
kizup <kizup.diman@gmail.com>
2018-11-21 20:39:37 +0400

    Initial commit

---
+++ app/src/main/java/ru/kizapp/yadiskclient/AppActivity.kt
@@ -0,0 +1,12 @@
+package ru.kizapp.yadiskclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app)
+    }
+}
